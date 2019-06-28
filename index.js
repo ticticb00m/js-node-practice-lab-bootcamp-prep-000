0fd832dc1dc860577ed12f8e711b18217163f173
mohamad farahmandifard <ticticb80om@gmail.com>
2019-06-28 19:40:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 var name = "Joe"
 var height = "74"
 
-console.log("Name:")
-console.log(name)
+console.log("Name:") console.log(name)
 
-console.log("Height:")
-console.log(height)
+console.log("Height:") console.log(height)
